570e14e9e9aa3941ac6f6291814fa3d89f1fd816
Aditya <aweditya@gmail.com>
2023-02-17 10:26:57 +0530

    Updated to use pr_ wrappers

--- general/crash.c
+++ general/crash.c
@@ -1,16 +1,22 @@
+#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 
-int init_module(void)
+MODULE_AUTHOR("Aditya Sriram <aweditya@gmail.com>");
+MODULE_DESCRIPTION("Linux Kaleidoscope: Chapter 4, Question 3");
+MODULE_LICENSE("GPL v2");
+
+static int __init crash_init(void)
 {
     int *p = NULL;
-    printk(KERN_ALERT "%d", *p);
+    pr_alert("%d", *p);
     return 0;
 }
 
-void cleanup_module(void)
+static void __exit crash_exit(void)
 {
-    printk(KERN_INFO "Exiting module...\n");
+    pr_info("Exiting module...\n");
 }
 
-MODULE_AUTHOR("Sukrit Bhatnagar <skrtbhtngr@gmail.com>");
-MODULE_LICENSE("GPL v2");
+module_init(crash_init);
+module_exit(crash_exit);
