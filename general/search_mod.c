970cff8148f39c27632f5246300940afd83d8004
Aditya <aweditya@gmail.com>
2023-03-29 19:02:33 +0530

    Updated to use pr_info

--- general/search_mod.c
+++ general/search_mod.c
@@ -1,32 +1,38 @@
+#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/string.h>
 
-char *mname;
+MODULE_AUTHOR("Aditya Sriram <aweditya@gmail.com>");
+MODULE_DESCRIPTION("Linux Kaleidoscope: Chapter 4, Question 10");
+MODULE_LICENSE("GPL v2");
 
-int __init init_module(void)
+static char *module_name;
+module_param(module_name, charp, 0660);
+MODULE_PARM_DESC(module_name, "Name of module to search for");
+
+static int __init searchmod_init(void)
 {
     struct module *m;
 
-    if(mname == NULL)
+    if(module_name == NULL)
         return -1;
 
     list_for_each_entry(m, &THIS_MODULE->list, list)
     {
-        if(!strcmp(m->name, mname))
+        if(!strcmp(m->name, module_name))
         {
-            printk(KERN_INFO "Name: %s\n", m->name);
+            pr_info("Name: %s\n", m->name);
             m->init();
         }
     }
     return 0;
 }
-void __exit cleanup_module(void)
+
+static void __exit searchmod_exit(void)
 {
     printk(KERN_INFO "Exiting module...\n");
 }
 
-module_param(mname, charp, 00600);
-MODULE_PARM_DESC(mname, "a string");
-
-MODULE_AUTHOR("Sukrit Bhatnagar <skrtbhtngr@gmail.com>");
-MODULE_LICENSE("GPL v2");
+module_init(searchmod_init);
+module_exit(searchmod_exit);
